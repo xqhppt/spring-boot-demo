d8eac2c008dae05e891bdaee8560a072e3a8f4f6
xuquanhao315 <xuquanhao315@tops001.com>
2018-01-23 18:40:41 +0800

    默认spring mvc示例

--- demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
+++ demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
@@ -1,47 +1,13 @@
 package com.xqh.demo.web.api.test;
 
-import com.xqh.demo.service.test.TestService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-/**
- * Created by xqh on 2017/10/3.
- */
-@Path("/test")
-//@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-@Produces({MediaType.APPLICATION_JSON})
-//@Produces({MediaType.APPLICATION_XML})
-@Controller
-@Api(tags = "测试", description = "测试相关")
+@RestController
 public class TestController {
-
-    @Autowired
-    private TestService testService;
-
-    @GET
-    @Path("/hi")
-    @ApiOperation(value = "hi", response = String.class)
-    public String hi() {
-        return testService.sayHello();
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello,World!";
     }
-
-//    @GET
-//    @Path("/json")
-//    @ApiOperation(value = "测试JSON", response = TestEntity.class)
-//    @Produces("application/json")
-//    public TestEntity testJson() {
-//        TestEntity testEntity = new TestEntity();
-//        testEntity.setId(11L);
-//        testEntity.setDesc("json object");
-//        testEntity.setDate(new Date());
-//
-//        return testEntity;
-//    }
 }
