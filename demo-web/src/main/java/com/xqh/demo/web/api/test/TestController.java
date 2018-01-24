03945a4e45e32d444d630df89ed238b3809fe377
xuquanhao315 <xuquanhao315@tops001.com>
2018-01-24 20:12:37 +0800

    spring mvc示范

--- demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
+++ demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
@@ -1,13 +1,28 @@
 package com.xqh.demo.web.api.test;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.xqh.demo.web.api.test.vo.HelloVo;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
-    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @RequestMapping(path = "/hello",method = RequestMethod.GET)
     public String sayHello() {
         return "Hello,World!";
+    }
+
+    @RequestMapping(path = "/hello/{name}",method = RequestMethod.GET)
+    public String sayHelloTo(@PathVariable("name") String name){
+        return "Hello," + name;
+    }
+
+    @RequestMapping(path = "/hello",method = RequestMethod.POST,consumes = "application/json")
+    public HelloVo addHello(@RequestBody HelloVo helloVo){
+        return helloVo;
+    }
+
+    @RequestMapping(path = "/msg",method = RequestMethod.GET,params = {"p1","p2=2"})
+    public String getMsg(){
+        return "请求参数包含p1，p2=2才可以访问";
     }
 }
---
+++ demo-web/src/main/java/com/xqh/demo/web/api/test/vo/HelloVo.java
@@ -0,0 +1,26 @@
+package com.xqh.demo.web.api.test.vo;
+
+/**
+ * Created by xqh on 2018/1/24.
+ */
+public class HelloVo {
+    private String msg;
+
+    private String name;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
