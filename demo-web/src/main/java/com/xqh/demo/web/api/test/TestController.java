4e0e7534a4c2e3fc75c63d77417388bd863f8d59
xuquanhao315 <xuquanhao315@tops001.com>
2018-01-25 18:57:03 +0800

    @InitBinder

--- demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
+++ demo-web/src/main/java/com/xqh/demo/web/api/test/TestController.java
@@ -1,14 +1,25 @@
 package com.xqh.demo.web.api.test;
 
 import com.xqh.demo.web.api.test.vo.HelloVo;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @InitBinder
+    public void init(WebDataBinder binder){
+        binder.addCustomFormatter(new DateFormatter("yyyyMMdd"));
+    }
+
     @RequestMapping(path = "/hello",method = RequestMethod.GET)
-    public String sayHello() {
-        return "Hello,World!";
+    public String sayHello(@RequestParam("msg") String msg) {
+        return "Hello,World!" + msg;
     }
 
     @RequestMapping(path = "/hello/{name}",method = RequestMethod.GET)
@@ -24,5 +35,15 @@ public class TestController {
     @RequestMapping(path = "/msg",method = RequestMethod.GET,params = {"p1","p2=2"})
     public String getMsg(){
         return "请求参数包含p1，p2=2才可以访问";
+    }
+
+    @RequestMapping(path = "/entity",method = RequestMethod.GET)
+    public String getMsgFromHeader(HttpEntity<String> requestEntity){
+        return requestEntity.getHeaders().get("my").get(0);
+    }
+
+    @RequestMapping(path = "/date",method = RequestMethod.GET)
+    public Date getDate(@RequestParam("date") Date date){
+        return date;
     }
 }
