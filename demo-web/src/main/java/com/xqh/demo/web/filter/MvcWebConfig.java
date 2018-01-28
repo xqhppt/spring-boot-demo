5ff4c3099f542f7ec34d457088710054c601ef95
xuquanhao315 <xuquanhao315@tops001.com>
2018-01-28 12:04:44 +0800

    处理返回值string

--- demo-web/src/main/java/com/xqh/demo/web/filter/MvcWebConfig.java
+++ demo-web/src/main/java/com/xqh/demo/web/filter/MvcWebConfig.java
@@ -1,9 +1,13 @@
 package com.xqh.demo.web.filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * Created by xqh on 2018/1/28.
@@ -16,5 +20,13 @@ public class MvcWebConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
 
         registry.addInterceptor(new MyTestInterceptors());
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+
+        //HttpMessageConverter实例集合中StringHttpMessageConverter要比其他Converts靠前
+        converters.add(0, new MappingJackson2HttpMessageConverter());
     }
 }
